05ec752f37d6fe76d2274b3bd2d16f4f6cbec14d
omkar1103 <115880514+omkar1103@users.noreply.github.com>
2025-01-15 18:09:48 +0530

    Excessive code removed from JS script

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -24,29 +24,17 @@ document.querySelector('.check').addEventListener('click', function () {
         }
 
     }
+    else if(guess!==secretNumber){
+        if(score>1){
+            document.querySelector('.message').textContent = guess>secretNumber ?' 📈Too High':'📉Too Low';
+            score=score-1;  
+            document.querySelector('.score').textContent=score; 
+        }
+        else{
+            document.querySelector('.message').textContent = ' 👎You Lost the Game';
+        }
+    }
 
-    else if(guess>secretNumber){
-        if(score>0){
-        document.querySelector('.message').textContent = ' 📈Too High';
-        score=score-1;  
-        document.querySelector('.score').textContent=score; 
-    }
-    else{
-        document.querySelector('.message').textContent = ' 👎You Lost the Game';
-    }
-}
-
-    else{
-        if(score>0){
-        document.querySelector('.message').textContent = '📉Too Low';
-        score=score-1;
-        document.querySelector('.score').textContent=score; 
-    }
-    else{
-        document.querySelector('.message').textContent = ' 👎You Lost the Game';
-
-    }
-}
 
 });
 
